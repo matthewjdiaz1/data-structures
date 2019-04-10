11c7db963dd12227bd7433ef70cefd907ce15b74
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2019-04-10 15:30:47 -0700

    coded stack.js and queue.js

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,4 +1,4 @@
-var Queue = function() {
+var Queue = function () {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
@@ -6,13 +6,41 @@ var Queue = function() {
 
   // Implement the methods below
 
-  someInstance.enqueue = function(value) {
+  someInstance.enqueue = function (value) {
+    storage[Object.keys(storage).length + 1] = value;
+    console.log(storage);
   };
 
-  someInstance.dequeue = function() {
+  someInstance.dequeue = function () {
+    // save first value to be returned & edge case
+    if (Object.keys(storage).length === 0) return;
+    var output = storage[1];
+    // transfer all values to the previous key
+    var temp = storage[1]; // a
+    for (let i = 1; i <= Object.keys(storage).length; i++) {
+      storage[i] = storage[i + 1]; // b
+    }
+    // delete the last key with now empty value
+    delete storage[Object.keys(storage).length];
+    // return saved first  value
+    return output;
+
+
+
+
+    // var output = storage[1];
+    // for (const key in storage) {
+    //   storage[key - 1] = storage[key]
+    // }
+    // // console.log('before', storage)
+    // // console.log('storage[:', Object.keys(storage).length - 1)
+    // delete storage[Object.keys(storage).length - 1]
+    // // console.log('after', storage);
+    // return output;
   };
 
-  someInstance.size = function() {
+  someInstance.size = function () {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
