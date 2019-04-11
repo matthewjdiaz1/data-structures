684f4c277ab0224a3b5bae27df91444adf4aee08
matthewjdiaz1 <matthewjdiaz1@gmail.com>
2019-04-11 15:45:30 -0700

    completed linkedList.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,15 +3,39 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+  // .addToTail() method, takes a value and adds it to the end of the list
   list.addToTail = function(value) {
+    if (list.head === null){
+      list.head = list.tail = Node(value);
+    } else {
+      var oldTail = list.tail;
+      list.tail = Node(value);
+      oldTail.next = list.tail;
+    }
   };
 
+  // .removeHead() method, removes the first node from the list and returns its value
   list.removeHead = function() {
+    var output = list.head.value;
+    list.head = list.head.next;
+    return output;
   };
 
+  // .contains() method, returns boolean reflecting whether 
+  // or not the passed-in value is in the linked list
   list.contains = function(target) {
+    var containsTarget = false;
+    var checkNode = function(node) {
+      if (node.next === null) { return containsTarget }
+      if (node.value === target) {
+        containsTarget = true;
+      } else {
+        checkNode(node.next);
+      }
+    }
+    checkNode(list.head);
+    return containsTarget;
   };
-
   return list;
 };
 
